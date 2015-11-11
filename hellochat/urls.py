7512bae1beeb9b69dda6f9ce24aad7cf337a8ff5
shabeer <shabeersha33@gmail.com>
2015-11-12 00:00:59 +0530

    login added

--- hellochat/urls.py
+++ hellochat/urls.py
@@ -18,4 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$','django.contrib.auth.views.login',{'template_name':'login.html'})
 ]
