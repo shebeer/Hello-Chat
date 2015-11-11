e5a01ed98a0f1cc69b2211dc1ef1b83073279aac
shebeer <shebeer.ma@acelrtech.com>
2015-11-11 16:47:25 +0530

    first push

---
+++ core/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    # This line is required. Links UserProfile to a User model instance.
+    user = models.OneToOneField(User)
+    # The additional attributes we wish to include.
+    picture = models.ImageField(upload_to='profile_images', blank=True,null=True)
+    mobile = models.CharField(max_length=15,blank=True,null=True)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
+
+class Message(models.Model):
+
+	message = models.CharField(max_length=500,blank=False,null=False)
+	created_on = models.DateTimeField(auto_now_add=True)
+	msg_from = models.ForeignKey(User,to_field='username',related_name='user_reated_to_msg_from')
+	msg_to = models.ForeignKey(User,to_field='username' ,related_name='user_reated_to_msg_to')
+	is_delivered = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=True)
+
+	def __unicode__(self):
+	    return "From:%s To:%s On:%s"%(self.msg_from.username,self.msg_to.username,str(created_on))
